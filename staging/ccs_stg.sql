b15c248a7b09a2e9f1f88b8e670a739817a89cbc
darshman83 <96759672+darshman83@users.noreply.github.com>
2023-09-27 14:32:02 -0400

    Create ccs_stg.sql

---
+++ staging/ccs_stg.sql
@@ -0,0 +1,7 @@
+---------------------------------------
+-- Stage CCS Table
+---------------------------------------
+CREATE OR REPLACE TABLE staging.ccs_stg AS
+SELECT * FROM ccs;
+
+SELECT * FROM ccs;
